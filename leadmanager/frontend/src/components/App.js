e3d3c60aafd019e06ca7ce2d45b8ca29b43b59d7
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-11-18 22:34:38 +0100

    remove comment

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-// import HomePage from "./HomePage";
 
 export default class App extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
 
   render() {
     return <h1>React App</h1>
