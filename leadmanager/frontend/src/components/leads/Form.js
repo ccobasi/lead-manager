10a18a979a0926ec92f2b8d064a18854f68faf67
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-11-22 14:03:11 +0100

    add leads folder

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    render() {
+        return (
+            <div>
+               <h2>Add Lead</h2>
+            </div>
+        )
+    }
+}
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Leads extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Lead List</h1>
+            </div>
+        )
+    }
+}
